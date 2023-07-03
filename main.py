e24a6574a8d54de1a3660e645d38c06b0e8ede42
Etyhae <Max.Mand@yandex.ru>
2023-07-03 17:06:53 +0300

    Added playlists search func

--- main.py
+++ main.py
@@ -19,15 +19,43 @@ opts = {'executable' : r"C:/ffmpeg/bin/ffmpeg.exe", 'source' : rf"C:\Users\maxma
 
 
 def song_search(songName, DoN=True): #download or not - Chech means track name in folder
+    global QUEUE
     search_result = clientYM.search(songName)
-    trackID = search_result['best']['result']['id']
-    albumID = search_result['best']['result']['albums'][0]['id']
-    trackFull = {trackID : albumID}
-    trackLabel = {'name' : search_result['best']['result']['title'],
-                   'author' : search_result['best']['result']['artists'][0]['name']}
-    if(DoN == True):
-        clientYM.tracks([trackFull])[0].download('0.mp3')
-    return trackLabel
+
+    def findTrack():
+        trackID = search_result['best']['result']['id']
+        albumID = search_result['best']['result']['albums'][0]['id']
+        trackFull = {trackID : albumID}
+        trackLabel = {'name' : search_result['best']['result']['title'],
+                    'author' : search_result['best']['result']['artists'][0]['name']}
+        QUEUE.append(trackLabel)
+        return trackFull
+    
+    def findPlaylist():
+        album = clientYM.users_playlists(search_result['best']['result']['kind'], search_result['best']['result']['uid'])
+        for tracks in album.tracks:
+            if album.track_count > 1:
+                trackLabel = {'name' : tracks['track']['title'],
+                                'author': tracks['track']['artists'][0]['name']}
+                QUEUE.append(trackLabel)
+    try:
+        if search_result['best']['type'] == 'track':
+            if DoN is True:
+                clientYM.tracks([findTrack()])[0].download('0.mp3') 
+            else:
+                findTrack()
+
+        elif search_result['best']['type'] == 'playlist':
+            findPlaylist()
+            songTitle = QUEUE[0]['name'] + " " + QUEUE[0]['author']
+            search_result = clientYM.search(songTitle)
+            if DoN is True:
+                clientYM.tracks([findTrack()])[0].download('0.mp3')
+            else:
+                findTrack() # cut this part
+    except: # error as e
+        pass
+        # return e
 
 @bot.event
 async def on_ready():
@@ -40,15 +68,14 @@ async def play(ctx, *args):
     global QUEUE
     global opts
     loopIs = loopIs
-    #QUEUE = QUEUE
     arguments = ', '.join(args)
     user = ctx.author
-    channel = user.voice.channel
+    channel = user.voice.channel # check if None
     voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
 
 
     async def firstplay():
-        QUEUE.append(song_search(arguments, True))
+        song_search(arguments, True)
         voice_channel = user.voice.channel
         if voice_channel != None: #
             vc = await channel.connect(self_deaf=True)
@@ -66,22 +93,6 @@ async def play(ctx, *args):
                     await ctx.send("Плейлист пуст")
         else:
             await ctx.send("Вы не находитесь в голосовом канале")
-
-    async def playsong():
-        QUEUE.append(song_search(arguments, True))
-        voice.stop()
-        song_search(QUEUE[0]['name'] + " " + QUEUE[0]['author'], True)
-        voice.play(discord.FFmpegPCMAudio(**opts))
-        if loopIs:
-            await play(ctx)
-        else:
-            await ctx.send(f"Сейчас играет - {QUEUE[0]['name']} : {QUEUE[0]['author']}")
-        while voice.is_playing():
-            await asyncio.sleep(1)
-        else:
-            if QUEUE:
-                del QUEUE[0]
-                await play(ctx)
 
     async def repeatplay():
         voice.stop()
@@ -103,23 +114,13 @@ async def play(ctx, *args):
     if args and voice is None:
         await firstplay()
     elif (args and voice) and not QUEUE:
-        await playsong()
+        song_search(arguments, True)
+        await repeatplay()
     elif args and QUEUE:
-        QUEUE.append(song_search(arguments, False))
+        song_search(arguments, False)
         await ctx.send(f"{QUEUE[-1]['name']} : {QUEUE[-1]['author']} - добавлено в очередь")
     else:
         await repeatplay()
-    # if args:
-    #     if not skipIs:
-    #         if not QUEUE: #
-    #             await firstplay()
-    #         else:
-    #             QUEUE.append(song_search(arguments, False))
-    #             await ctx.send(f"{QUEUE[-1]['name']} : {QUEUE[-1]['author']} - добавлено в очередь")
-    #     else:
-    #         await repeatplay()
-    # else: #придумать как убрать повторения
-    #     await repeatplay()
 
 @bot.command()
 async def loop(ctx):
@@ -133,13 +134,17 @@ async def loop(ctx):
 @bot.command()
 async def playlist(ctx):
     global QUEUE
-    playlist = ""
+    songPlaylist = ""
     for x in QUEUE:
-        playlist += f"{x['name']} : {x['author']}\n"
-    if playlist == "":
-        await ctx.send("Плейлист пуст")
+        songPlaylist += f"{x['name']} : {x['author']}\n"
+        if len(songPlaylist) > 600:
+            await ctx.send(songPlaylist + f"... и еще {len(QUEUE)-QUEUE.index(x)}")
+            break
     else:
-        await ctx.send(playlist)
+        if not songPlaylist :
+            await ctx.send("Плейлист пуст")
+        else:
+            await ctx.send(songPlaylist)
 
 @bot.command()
 async def pause(ctx):
